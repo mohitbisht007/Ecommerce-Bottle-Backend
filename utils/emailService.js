9fb0d4149ca08a76b21e28d6677afb33b82bff48
Mohit <mohitbisht5678@gmail.com>
2026-06-30 11:24:22 +0530

    changed trapoter settings

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,16 +1,27 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.error("SMTP VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP Server is ready.");
+  }
 });
 
 export const sendOrderEmail = async (email, order, status) => {
